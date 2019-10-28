7041d2392f744a7522d111291fc69125d4490b16
CPOServices <cposervices2020@gmail.com>
2019-10-28 18:38:32 -0500

    Desarrollo del FrontEnd

---
+++ Soporte/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+//Rutas
+import { APP_ROUTES } from './app.routes';
+
+//Modulos
+import { PagesModule } from './pages/pages.module';
+
+//Servicios
+import { ServiceModule } from './services/service.module';
+
+//Componentes
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTES,
+    PagesModule,
+    ServiceModule
+  ],
+  providers: [
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Soporte/src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+
+
+@NgModule({
+    imports: [
+        RouterModule,
+        CommonModule
+    ],
+    declarations:[
+       HeaderComponent,
+        BreadcrumbComponent,
+        SidebarComponent,
+        NopagefoundComponent
+    ],
+    exports:[
+        HeaderComponent,
+        BreadcrumbComponent,
+        SidebarComponent,
+        NopagefoundComponent
+    ]
+})
+export class SharedModule { }
